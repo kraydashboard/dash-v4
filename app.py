c98b73398f81295f2bc9f0ff53bbb2fdfe2eb936
kraydashboard <kraydashboard@tutamail.com>
2026-05-17 16:23:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1668,4 +1668,4 @@ def delete_log():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
